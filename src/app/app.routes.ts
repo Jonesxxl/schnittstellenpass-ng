5f86aa0bb67c4aefe614ec7edc488d7b3ce46524
Jonesxxl <Lukasxx95.s@gmail.com>
2025-11-20 23:36:51 +0100

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'design',
+    pathMatch: 'full'
+  },
+  {
+    path: 'design',
+    loadComponent: () => import('./design/design.component').then(m => m.LandingComponent)
+  },
+  {
+    path: 'episodes',
+    loadComponent: () => import('./episodes/episodes.component').then(m => m.EpisodesComponent)
+  },
+  {
+    path: 'landing',
+    loadComponent: () => import('./landing/landing.component').then(m => m.LandingPageComponent)
+  }
+];
